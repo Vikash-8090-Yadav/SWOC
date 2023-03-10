fd90692d60394572a58775884fcfc326e8928348
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-03-10 22:58:28 +0530

    added

--- Component/Layout.js
+++ Component/Layout.js
@@ -4,16 +4,15 @@ import AboutApp from "./AboutApp";
 import Test from "./Test";
 import Contact from "./Contact";
 import Footer from "./Footer";
-import Head from "next/Head"
 export default function Layout({children}) {
 
   console.log(children);
   return (
     <>
-      <Head>
+      <div>
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-      </Head>
+      </div>
       
       <div className = "">
         <Carousel/> 
